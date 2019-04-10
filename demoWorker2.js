509a2f09ce2821d1778d94130249e23cba607067
TheForaneo <general_dragon117@hotmail.com>
2019-04-10 13:50:34 -0600

    WebWorkers

---
+++ demoWorker2.js
@@ -0,0 +1,9 @@
+var i = 1000;
+function cuenta(){
+	i=i-1;
+	//Envia respuesta al worker que lo mando llamar
+	postMessage(i);
+	//Ejecutamos cuenta() cada 500 milisegundos
+	setTimeout("cuenta()",500);
+}
+cuenta();
