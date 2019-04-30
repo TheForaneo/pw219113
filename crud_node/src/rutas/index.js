24324d102d82408a359748f9df9f9c0cc84ca003
TheForaneo <general_dragon117@hotmail.com>
2019-04-30 13:58:19 -0600

    Listado de clientes y controlador

--- crud_node/src/rutas/index.js
+++ crud_node/src/rutas/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const rutas = express.Router();
 
-rutas.get("/", (req,res) => {
-	res.send("<marquee behavior=alternate scrollamount=50>Hola mundo en node huashuas</marquee>");
-});
+const customerController = require('../controllers/customerController');
+
+//rutas.get("/", (req,res) => {
+//	res.send("<marquee behavior=alternate scrollamount=50>Hola mundo en node huashuas</marquee>");
+//});
+
+rutas.get('/', customerController.list);
+//rutas.post('/add', customerController.save);
+//rutas.get('/delete', customerController.delete);
+//rutas.get('/update/:id', customerController.edit);
+//rutas.post('/update/:id', customerController.update);
 
 module.exports = rutas;
