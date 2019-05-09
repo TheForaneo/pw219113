d7db3c0fa8e9b97d01908e90cfcee460f00d1e1a
TheForaneo <general_dragon117@hotmail.com>
2019-05-09 13:59:56 -0600

    Fruta2 vue.js

---
+++ vues_ejemplos/jls/primervue.js
@@ -0,0 +1,37 @@
+const app = new Vue({
+	el: '#app',
+	data: {
+		titulo: 'Hola mundo pero con Vue',
+		frutas: ['Manzana','Uva','Peras','Fresas'],
+		frutas2: [
+			{nombre: 'Manzana', cantidad: 11},
+			{nombre: 'Uva', cantidad: 0},
+			{nombre: 'Peras', cantidad: 80},
+			{nombre: 'Fresas', cantidad: 110}	
+		],
+		nuevaFruta: '',
+		total: 0
+	},
+	methods: {
+		agregarFruta(){
+			//console.log("diste clic");
+			if(this.nuevaFruta != ''){
+			this.frutas.push(this.nuevaFruta);
+			this.frutas2.push({
+				nombre: this.nuevaFruta,
+				cantidad: 0
+			});}
+			this.nuevaFruta = '';
+		}
+	},
+	computed: {
+		sumarFrutas(){
+			this.total = 0;
+			for(fruta of this.frutas2){
+				this.total=this.total+fruta.cantidad;
+				// this.total+=fruta.cantidad;
+			}
+			return this.total;
+		}
+	}
+});
